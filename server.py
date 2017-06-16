842a954aec25f6bae54a72b7ebf431a4fe48f08d
Akira Kido <kido.akira@gmail.com>
2017-06-16 13:34:36 +0000

    port numbr is a variable.

--- server.py
+++ server.py
@@ -3,19 +3,20 @@
 Server Program Ver.0.0.3
  Time-stamp: <2017-05-13 17:43:34 akira>
 """
+import sys
 import datetime
 import logging
 import socket
 
-def log_open():
+def log_open(basename):
     today = datetime.datetime.today()
-    logging.basicConfig(filename=today.strftime('access_%Y%m%d_%H%M%S.log'),
+    logfile=today.strftime(basename + '_%Y%m%d_%H%M%S.log')
+    logging.basicConfig(filename=logfile,
                         format='%(asctime)s:%(levelname)s:%(message)s',
                         level=logging.DEBUG)
 
-def server_routine():
+def server_routine(port):
     host = socket.gethostname()
-    port = 60606
     serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serversock.bind((host, port))
     serversock.listen(10)
@@ -32,5 +33,10 @@ def server_routine():
     logging.info('Server End')
 
 if __name__ == '__main__':
-    log_open()
-    server_routine()
+	if len(sys.argv) < 3:
+		print 'usage: python %s port_number log_base_name' % sys.argv[0]
+		quit()
+	port = int(sys.argv[1])
+	basename = sys.argv[2]
+	log_open(port)
+	server_routine(basename)
